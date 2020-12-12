d2ec509d9b2d771a2ea8b20f4d423a4b7aff9336
ReyS0926 <rey.sosa26@gmail.comm>
2020-12-12 15:54:12 -0500

    Done

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,26 +3,50 @@ import csv
 
 csvpath = os.path.join('./Resources/budget_data.csv')
 
-Date = []
+Month = []
+Profit_Losses = []
+Avg_Change = []
 
-with open(csvpath, newline='', encoding='utf-8') as csvfile:
+
+with open(csvpath) as csvfile:
     
     csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
 
     for row in csvreader:
   
     # The total number of months included in the dataset
-        Date.append(row)
-    print(len(Date))
-    
+        Month.append(row[0])
     
     #The net total amount of "Profit/Losses" over the entire period
-
-
+        Profit_Losses.append(int(row[1]))
+    
     #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
+    for r in range(len(Profit_Losses)-1):
+        Avg_Change.append(Profit_Losses[r+1]-Profit_Losses[r])
 
     #The greatest increase in profits (date and amount) over the entire period
+max_increase_value = max(Avg_Change)
+max_decrease_value = min(Avg_Change)
+max_increase_month = Avg_Change.index(max(Avg_Change)) + 1
+max_decrease_month = Avg_Change.index(min(Avg_Change)) + 1 
 
+print("Financial Analysis")
+print("--------------------------------")
+print(f"The total number of months included in the dataset is {(len(Month))}")
+print(f"The net total amount of Profit/Losses over the entire period is ${(sum(Profit_Losses)):,.2f}")
+print(f"Average Change: ${round(sum(Avg_Change)/len(Avg_Change),2):,.2f}")
+print(f"Greatest Increase in Profits: {Month[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {Month[max_decrease_month]} (${(str(max_decrease_value))})")
 
-    #The greatest decrease in losses (date and amount) over the entire period
+output_file = os.path.join("Analysis","analysis.txt")
+
+with open(output_file, "w", newline="") as datafile:
+    datafile.write("Financial Analysis\n")
+    datafile.write("--------------------------------\n")
+    datafile.write(f"The total number of months included in the dataset is {(len(Month))}")
+    datafile.write(f"The net total amount of Profit/Losses over the entire period is ${(sum(Profit_Losses)):,.2f}")
+    datafile.write(f"Average Change: ${round(sum(Avg_Change)/len(Avg_Change),2):,.2f}")
+    datafile.write(f"Greatest Increase in Profits: {Month[max_increase_month]} (${(str(max_increase_value))})")
+    datafile.write(f"Greatest Decrease in Profits: {Month[max_decrease_month]} (${(str(max_decrease_value))})")
